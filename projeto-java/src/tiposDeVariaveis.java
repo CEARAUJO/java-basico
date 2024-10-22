cc7a84ce50a5ae29d296cbe7b2a345c127308787
Carlos Eduardo Lopes de Araujo <carlos.araujo@elotech.localnet>
2024-10-22 13:10:50 -0300

    aula de variaveis

---
+++ projeto-java/src/tiposDeVariaveis.java
@@ -0,0 +1,22 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        //AULA 1 - TIPOS E VARIAVEIS
+        byte numberByte = ; //range -128 at 127
+        short numberShort; //range -32.768 at 32.767
+        int numberInt; //range -2.147.483.648 at 2.147.483.647
+        long numberLong; //range -9.223.372.036.854.775.808 at 9.223.372.036.854.775.807
+
+        double numberDouble; //range -1,7976E + 308 at 1,7976E + 308, prec 15 dig MORE USED
+        float numberFloat; //range -3,4028E + 38 at 3,4028E + 38, prec 6 – 7 díg
+
+        boolean trueOrFalse;
+
+        char wordChar;
+        String wordString;
+
+        final double NUMBER_CONST_DOUBLE = 3,14;
+
+        NUMBER_CONST_DOUBLE = 3,15;
+    }
+}
