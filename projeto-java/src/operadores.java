72e08bb531012d46bce4ea0f34fc73d15c150064
Carlos Eduardo Lopes de Araujo <carlos.araujo@elotech.localnet>
2024-10-28 10:00:17 -0300

    Variáveis java

---
+++ projeto-java/src/operadores.java
@@ -0,0 +1,6 @@
+public class operadores {
+    public static void main(String[] args) throws Exception {
+        int numeroSoma = 1 + 2;
+        System.out.write(numeroSoma);
+    }
+}
--- projeto-java/src/tiposDeVariaveis.java
+++ projeto-java/src/tiposDeVariaveis.java
@@ -1,22 +1,33 @@
-public class App {
+public class tiposDeVariaveis {
     public static void main(String[] args) throws Exception {
 
         //AULA 1 - TIPOS E VARIAVEIS
-        byte numberByte = ; //range -128 at 127
-        short numberShort; //range -32.768 at 32.767
-        int numberInt; //range -2.147.483.648 at 2.147.483.647
-        long numberLong; //range -9.223.372.036.854.775.808 at 9.223.372.036.854.775.807
+        byte numberByte = 126; //range -128 at 127
+        short numberShort = 32766; //range -32.768 at 32.767
+        int numberInt = 2147483646; //range -2.147.483.648 at 2.147.483.647
+        long numberLong = 9223372036854775807L; //range -9.223.372.036.854.775.808 at 9.223.372.036.854.775.807 //Necessário colocar o L ao final do numero
 
-        double numberDouble; //range -1,7976E + 308 at 1,7976E + 308, prec 15 dig MORE USED
-        float numberFloat; //range -3,4028E + 38 at 3,4028E + 38, prec 6 – 7 díg
+        double numberDouble = 3.822; //range -1,7976E + 308 at 1,7976E + 308, prec 15 dig MORE USED
+        float numberFloat = 3.822F; //range -3,4028E + 38 at 3,4028E + 38, prec 6 – 7 díg //necessário colocar F ao final do numero
 
-        boolean trueOrFalse;
+        boolean trueOrFalse = true;
 
-        char wordChar;
-        String wordString;
+        char wordChar = 'P';
+        String wordString = "Palabras";
 
-        final double NUMBER_CONST_DOUBLE = 3,14;
+        final double NUMBER_CONST_DOUBLE = 3.14;
 
-        NUMBER_CONST_DOUBLE = 3,15;
+        System.out.println(numberByte);
+        System.out.println(numberShort);
+        System.out.println(numberInt);
+        System.out.println(numberLong);
+        System.out.println(numberDouble);
+        System.out.println(numberFloat);
+        System.out.println(trueOrFalse);
+        System.out.println(wordChar);
+        System.out.println(wordString);
+        System.out.println(NUMBER_CONST_DOUBLE);
+
+        //NUMBER_CONST_DOUBLE = 3.15;
     }
 }
